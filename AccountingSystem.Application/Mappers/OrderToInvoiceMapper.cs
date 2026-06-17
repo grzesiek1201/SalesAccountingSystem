1564dd13a5541e1cccf4e4250957dfd3503e734a
grzesiek1201 <gzywicki@gmail.com>
2026-06-17 22:00:12 +0200

    Add Order to Invoice mapping with issue and due date logic

---
+++ AccountingSystem.Application/Mappers/OrderToInvoiceMapper.cs
@@ -0,0 +1,36 @@
+﻿using AccountingSystem.Domain.Entities;
+using AccountingSystem.Domain.Enums;
+using Microsoft.VisualBasic;
+using System.Linq;
+
+namespace AccountingSystem.Application.Mappers
+{
+    public class OrderToInvoiceMapper
+    {
+        public Invoice Map(Order order)
+        {
+            if (order == null)
+                return null;
+
+            var invoice = new Invoice
+            {
+                CustomerId = order.CustomerId,
+                DateCreated = DateTime.Now,
+                IssueDate = DateTime.Now,
+                DueDate = DateTime.Now.AddDays(14),
+                Status = InvoiceStatus.Draft,
+                OrderId = order.Id,
+                Items = order.Items.Select(q => new InvoiceItem
+                {
+                    ProductId = q.ProductId,
+                    Quantity = q.Quantity,
+                    BaseUnitPrice = q.BaseUnitPrice,
+                    DiscountPercent = q.DiscountPercent,
+                    Position = q.Position
+                }).ToList()
+            };
+
+            return invoice;
+        }
+    }
+}
