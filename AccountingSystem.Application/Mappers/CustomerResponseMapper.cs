3d8a3b62feb9017a69c519dd101dcf508d5a17ec
grzesiek1201 <gzywicki@gmail.com>
2026-06-25 14:35:27 +0200

    feat(application): add response mappers for customer and product

---
+++ AccountingSystem.Application/Mappers/CustomerResponseMapper.cs
@@ -0,0 +1,21 @@
+﻿using AccountingSystem.Application.DTOs.Customers;
+using AccountingSystem.Domain.Entities;
+
+namespace AccountingSystem.Application.Mappers
+{
+    public class CustomerResponseMapper
+    {
+        public CustomerResponse Map(Customer c)
+        {
+            return new CustomerResponse
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Email = c.Email,
+                Street = c.Street,
+                City = c.City,
+                ZipCode = c.ZipCode
+            };
+        }
+    }
+}
---
+++ AccountingSystem.Application/Mappers/ProductResponseMapper.cs
@@ -0,0 +1,20 @@
+﻿using AccountingSystem.Application.DTOs.Products;
+using AccountingSystem.Domain.Entities;
+
+namespace AccountingSystem.Application.Mappers
+{
+    public class ProductResponseMapper
+    {
+        public ProductResponse Map(Product p)
+        {
+            return new ProductResponse
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Price = p.Price,
+                CategoryId = p.CategoryId
+            };
+        }
+    }
+}
+
